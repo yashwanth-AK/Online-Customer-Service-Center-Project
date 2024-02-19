7f794bdfd0e899e0b712ec382cb8a7a0a3782407
Duraisamy <NDURAISA@ford.com>
2024-02-19 18:25:25 +0530

    first commit-added functionalities

--- src/main/java/com/onlineCustomerServiceCenter/issue/IssueController.java
+++ src/main/java/com/onlineCustomerServiceCenter/issue/IssueController.java
@@ -1,7 +1,13 @@
 package com.onlineCustomerServiceCenter.issue;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IssueController {
+    //No endpoints for Issue module. It is to be linked with other modules
+
 }
--- src/main/java/com/onlineCustomerServiceCenter/issue/IssueRepository.java
+++ src/main/java/com/onlineCustomerServiceCenter/issue/IssueRepository.java
@@ -1,4 +1,12 @@
 package com.onlineCustomerServiceCenter.issue;
 
-public interface IssueRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IssueRepository extends JpaRepository<Issue, Integer> {
+
+    //public List<Issue> findAllByType(String type);
 }
