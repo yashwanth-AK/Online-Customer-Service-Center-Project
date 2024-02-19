1f4cff242c8481781f6cf02f94aa0314be1876cd
Karthick <karthicksp20@gmail.com>
2024-02-19 12:52:11 +0530

    Branch-Updated

--- src/main/java/com/onlineCustomerServiceCenter/customer/Customer.java
+++ src/main/java/com/onlineCustomerServiceCenter/customer/Customer.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
