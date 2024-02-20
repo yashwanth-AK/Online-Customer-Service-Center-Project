972c6442d5a7dd1994283ad05ddf2cb3729ddd9b
R <GR20@ford.com>
2024-02-20 10:10:09 +0530

    acceptSolution API added

--- src/main/java/com/onlineCustomerServiceCenter/solution/SolutionService.java
+++ src/main/java/com/onlineCustomerServiceCenter/solution/SolutionService.java
@@ -2,4 +2,5 @@ package com.onlineCustomerServiceCenter.solution;
 
 public interface SolutionService {
    Solution createSolution(String solutionDescription);
+   Solution acceptSolution(String  solutionId);
 }
